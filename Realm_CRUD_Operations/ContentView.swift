31676b5eb1c100bde25c6d6130de66aa3a3c8364
Max <maxim.macari.dam@gmail.com>
2021-01-05 00:47:27 +0100

    Initial Commit

---
+++ Realm_CRUD_Operations/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Realm_CRUD_Operations
+//
+//  Created by Maxim Macari on 5/1/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Realm_CRUD_Operations/Realm_CRUD_OperationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Realm_CRUD_OperationsApp.swift
+//  Realm_CRUD_Operations
+//
+//  Created by Maxim Macari on 5/1/21.
+//
+
+import SwiftUI
+
+@main
+struct Realm_CRUD_OperationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
